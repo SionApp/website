59fb1ec767d62bb25bfa641273011792ee5b3579
Danzt <danztty@gmail.com>
2024-03-21 20:41:00 -0400

    feat: Add project landing

---
+++ astro.config.ts
@@ -0,0 +1,49 @@
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
+import { defineConfig } from "astro/config"
+
+import sitemap from "@astrojs/sitemap"
+import { VitePWA } from "vite-plugin-pwa"
+
+import robotsTxt from "astro-robots-txt"
+
+// Helper imports
+import { manifest, seoConfig } from "./src/utils/seoConfig"
+
+// https://astro.build/config
+export default defineConfig({
+	prefetch: true,
+	devToolbar: {
+		enabled: false,
+	},
+	integrations: [tailwind(), sitemap(), robotsTxt()],
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
+	build: {
+		inlineStylesheets: "always",
+	},
+	output: "hybrid",
+	site: seoConfig.baseURL,
+	vite: {
+		build: {
+			cssMinify: "lightningcss",
+		},
+		ssr: {
+			noExternal: ["path-to-regexp"],
+		},
+		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest,
+				workbox: {
+					globDirectory: "dist",
+					globPatterns: ["**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}"],
+					// Don't fallback on document based (e.g. `/some-page`) requests
+					// This removes an errant console.log message from showing up.
+					navigateFallback: null,
+				},
+			}),
+		],
+	},
+})
---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly API_URL: string
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
+
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
