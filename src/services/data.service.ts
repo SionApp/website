86ccf10829e02ef163a592f74f4ae0ab7725bfd9
daniel.bonalde <daniel.bonalde@neilit.com>
2024-04-12 15:07:16 -0400

    chore: refactor carousel image

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -1,8 +1,14 @@
 import type { LandingPageData } from "@/config/landing.interface"
 import landingData from "@/data/landing.json"
+import eventsData from "@/data/events.json"
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const getLandingData = async (): Promise<LandingPageData> => {
 	const data: LandingPageData = landingData
 	return data
+}
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export const getEventsData = async () => {
+	return eventsData
 }
